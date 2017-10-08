d9953bd885ee910710c07bc59ae1795cc2b74abf
Naazim <naazim4u@gmail.com>
2017-10-08 22:54:34 +0200

    Initial commit

---
+++ dist/app.bundle.js
@@ -0,0 +1,115 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+throw new Error("Cannot find module \"jquery\"");
+
+
+__WEBPACK_IMPORTED_MODULE_0_jquery___default()(function(){
+    /**
+     * When navigation menu is clicked
+     */
+    __WEBPACK_IMPORTED_MODULE_0_jquery___default()('nav a').on('click', function(){
+        if(__WEBPACK_IMPORTED_MODULE_0_jquery___default()(this).hasClass('hamburger-icon')) { //For mobile view
+            __WEBPACK_IMPORTED_MODULE_0_jquery___default()(this).toggleClass('active').siblings().removeClass('active');;
+        }
+        else {  //For desktop view
+            __WEBPACK_IMPORTED_MODULE_0_jquery___default()(this).addClass('active').siblings().removeClass('active');
+        }
+    });
+
+    /**
+     * Filter the cards based on the checkbox clicked
+     */
+    __WEBPACK_IMPORTED_MODULE_0_jquery___default()('.filters [type=checkbox]').on('change', function(){
+        var selected = __WEBPACK_IMPORTED_MODULE_0_jquery___default()(this).attr('id');  //fetch the id of the checked filter
+        __WEBPACK_IMPORTED_MODULE_0_jquery___default()(`.content [data-type=${selected}]`).fadeToggle(); //hide the corresponding fruit card
+    });
+});
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+module.exports = __webpack_require__(0);
+
+
+/***/ })
+/******/ ]);
---
+++ src/app.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+
+$(function(){
+    /**
+     * When navigation menu is clicked
+     */
+    $('nav a').on('click', function(){
+        if($(this).hasClass('hamburger-icon')) { //For mobile view
+            $(this).toggleClass('active').siblings().removeClass('active');;
+        }
+        else {  //For desktop view
+            $(this).addClass('active').siblings().removeClass('active');
+        }
+    });
+
+    /**
+     * Filter the cards based on the checkbox clicked
+     */
+    $('.filters [type=checkbox]').on('change', function(){
+        var selected = $(this).attr('id');  //fetch the id of the checked filter
+        $(`.content [data-type=${selected}]`).fadeToggle(); //hide the corresponding fruit card
+    });
+});
